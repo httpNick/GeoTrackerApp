10db7e08d7a3bb4c92d98bb26c57577fb23dcc76
mmills13 <mmills13@uw.edu>
2015-04-13 18:28:24 -0700

    just testing Git commit....MM

--- app/src/androidTest/java/com/example/httpnick/geotracker/ApplicationTest.java
+++ app/src/androidTest/java/com/example/httpnick/geotracker/ApplicationTest.java
@@ -10,5 +10,5 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
-    //just testing commit
+    //just testing commit ...mm
 }
