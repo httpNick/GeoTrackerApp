8d486e56b8564da6a737cdf20212b3e283f1ce0e
Nick Duncan <nick.b.duncan@gmail.com>
2015-04-16 18:23:21 -0700

    Can go to register page.

--- app/src/main/java/com/example/httpnick/geotracker/MainActivity.java
+++ app/src/main/java/com/example/httpnick/geotracker/MainActivity.java
@@ -1,40 +1,29 @@
 package com.example.httpnick.geotracker;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 // This is a test commit to make sure git is working.
 public class MainActivity extends ActionBarActivity {
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        register = (Button) findViewById(R.id.Register);
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        System.out.println("test commit MM");
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), RegisterUser.class);
+                startActivity(i);
+            }
+        });
     }
 }
