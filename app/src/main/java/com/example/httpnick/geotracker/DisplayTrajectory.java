c8b6f5d69b0402073f89d0a21a484a7083968a09
Nick Duncan <nick.b.duncan@gmail.com>
2015-05-27 11:54:46 -0700

    Cleaned up / javadoc'd the DisplayTrajectory class.

--- app/src/main/java/com/example/httpnick/geotracker/DisplayTrajectory.java
+++ app/src/main/java/com/example/httpnick/geotracker/DisplayTrajectory.java
@@ -5,12 +5,7 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.view.View;
 import android.widget.ListView;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TableRow.LayoutParams;
-import android.widget.TextView;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -28,49 +23,72 @@ import java.util.Calendar;
 
 /**
  * Created by httpnick on 5/22/15.
+ * @author Nick Duncan
+ * An activity that displays locations/times that are stored for the currently
+ * logged in user. Puts all points through an adapter and adds it to the view for this
+ * acitivty.
  */
 public class DisplayTrajectory extends Activity {
 
     /** preferences for the current application */
     private SharedPreferences pref;
-
+    /** JSON array that points will be stored in from the web service.*/
     private JSONArray points;
-
-    private TableLayout table_layout;
-
+    /** ArrayList that will hold Location packages that will be used by the view adapter*/
     private ArrayList<LocationPackage> lps;
-
+    /** List view on the view that points will be displayed on.*/
     private ListView locationListView;
-
+    /** Adapter that will be used to go on the ListView.*/
     private LocationAdapter la;
 
     public void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
+
         setContentView(R.layout.activity_table);
-        lps = new ArrayList<LocationPackage>();
+
+        lps = new ArrayList<>();
+
         locationListView = (ListView) findViewById(R.id.loc_list_view);
+
         la = new LocationAdapter(this, lps);
+
         Bundle b = getIntent().getExtras();
-        //table_layout = (TableLayout) findViewById(R.id.tableLayout1);
-        //table_layout.setVisibility(View.INVISIBLE);
+        /** Gain a reference to the shared preferences.*/
         pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        // Start time from trajectory page
         ArrayList<Integer> startTime = b.getIntegerArrayList("startTime");
+        // Start date from trajectory page
         ArrayList<Integer> startDate = b.getIntegerArrayList("startDate");
+        // End time for the trajectory page
         ArrayList<Integer> endTime = b.getIntegerArrayList("endTime");
+        // end date from the trajectory page
         ArrayList<Integer> endDate = b.getIntegerArrayList("endDate");
+        // convert integers from trajectory page to a unix timestamp for start time.
         int startUnix = componentTimeToTimestamp(startDate.get(0), startDate.get(1), startDate.get(2),
                 startTime.get(0), startTime.get(1));
+        // convert integers from trajectory page to a unix timestamp for end time.
         int endUnix = componentTimeToTimestamp(endDate.get(0), endDate.get(1), endDate.get(2),
                 endTime.get(0), endTime.get(1));
-        System.out.println("START : " + startUnix + " END : " + endUnix);
+
         String DB_URL = "http://450.atwebpages.com/view.php?uid="+pref.getString("userid", "default")+
                 "&start="+startUnix+"&end="+endUnix;
+
         DownloadWebPageTask task = new DownloadWebPageTask();
+
         task.execute(new String[]{DB_URL});
 
     }
 
-    int componentTimeToTimestamp(int year, int month, int day, int hour, int minute) {
+    /**
+     * Takes in y/m/d/h/min and converts to a unix time stamp.
+     * @param year year input
+     * @param month month input
+     * @param day day input
+     * @param hour hour input
+     * @param minute minute input
+     * @return unix timestamp based on passed in parameters.
+     */
+    private int componentTimeToTimestamp(int year, int month, int day, int hour, int minute) {
 
         Calendar c = Calendar.getInstance();
         c.set(Calendar.YEAR, year);
@@ -82,117 +100,6 @@ public class DisplayTrajectory extends Activity {
         c.set(Calendar.MILLISECOND, 0);
 
         return (int) (c.getTimeInMillis() / 1000L);
-    }
-
-    private void fillTable() {
-        try {
-            TableRow headerRow = new TableRow(this);
-            headerRow.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT,
-                    LayoutParams.WRAP_CONTENT));
-            headerRow.setPadding(0, 0, 0, 2);
-
-            TableRow.LayoutParams headerLlp = new TableRow.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
-            headerLlp.setMargins(0, 0, 2, 0);//2px right-margin
-
-            headerRow.setLayoutParams(headerLlp);
-
-            TextView latHeader = new TextView(this);
-            latHeader.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-                    LayoutParams.WRAP_CONTENT));
-            latHeader.setPadding(0, 0, 4, 3);
-            latHeader.setText("Latitude");
-            headerRow.addView(latHeader);
-
-            TextView lonHeader = new TextView(this);
-            lonHeader.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-                    LayoutParams.WRAP_CONTENT));
-            lonHeader.setPadding(0, 0, 4, 3);
-            lonHeader.setText("Longitude");
-            headerRow.addView(lonHeader);
-
-            TextView speedHeader = new TextView(this);
-            speedHeader.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-                    LayoutParams.WRAP_CONTENT));
-            speedHeader.setPadding(0, 0, 4, 3);
-            speedHeader.setText("Speed");
-            headerRow.addView(speedHeader);
-
-            TextView headingHeader = new TextView(this);
-            headingHeader.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-                    LayoutParams.WRAP_CONTENT));
-            headingHeader.setPadding(0, 0, 4, 3);
-            headingHeader.setText("Heading");
-            headerRow.addView(headingHeader);
-
-            TextView timeHeader = new TextView(this);
-            timeHeader.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-                    LayoutParams.WRAP_CONTENT));
-            timeHeader.setPadding(0, 0, 4, 3);
-            timeHeader.setText("Time");
-            headerRow.addView(timeHeader);
-
-            table_layout.addView(headerRow);
-
-
-
-            JSONObject o;
-
-
-            for(int i = 0; i < points.length(); i++) {
-                o = points.getJSONObject(i);
-                TableRow row = new TableRow(this);
-                row.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT,
-                        LayoutParams.WRAP_CONTENT));
-                row.setPadding(0, 0, 0, 2);
-
-                TableRow.LayoutParams llp = new TableRow.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
-                llp.setMargins(0, 0, 2, 0);//2px right-margin
-
-                row.setLayoutParams(llp);
-
-                TextView lat = new TextView(this);
-                lat.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-                        LayoutParams.WRAP_CONTENT));
-                lat.setPadding(0, 0, 4, 3);
-                lat.setText((String) o.get("lat"));
-                row.addView(lat);
-
-                TextView lon = new TextView(this);
-                lon.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-                        LayoutParams.WRAP_CONTENT));
-                lon.setPadding(0, 0, 4, 3);
-                lon.setText((String) o.get("lon"));
-                row.addView(lon);
-
-                TextView speed = new TextView(this);
-                speed.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-                        LayoutParams.WRAP_CONTENT));
-                speed.setPadding(0, 0, 4, 3);
-                speed.setText((String) o.get("speed"));
-                row.addView(speed);
-
-
-                TextView heading = new TextView(this);
-                heading.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-                        LayoutParams.WRAP_CONTENT));
-                heading.setPadding(0, 0, 4, 3);
-                heading.setText((String) o.get("heading"));
-                row.addView(heading);
-
-
-                TextView time = new TextView(this);
-                time.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-                        LayoutParams.WRAP_CONTENT));
-                time.setPadding(0, 0, 4, 3);
-                time.setText(String.valueOf(o.get("time")));
-                row.addView(time);
-
-                table_layout.addView(row);
-            }
-            table_layout.setVisibility(View.VISIBLE);
-        } catch(JSONException j) {
-            j.printStackTrace();
-        }
     }
 
     /**
@@ -239,6 +146,10 @@ public class DisplayTrajectory extends Activity {
                 String pass = (String) obj.get("result");
                 if (pass.equals("success")) {
                     points = obj.getJSONArray("points");
+                    /**
+                     * Loop through the returned JSONArray and save all points into
+                     * LocationPackage objects to be used by the adapter.
+                     */
                     for(int i = 0; i < points.length(); i++) {
                         JSONObject o = points.getJSONObject(i);
                         LocationPackage lp = new LocationPackage(o.getString("heading"),
@@ -247,7 +158,6 @@ public class DisplayTrajectory extends Activity {
                         lps.add(lp);
                     }
 
-                    //fillTable();
                     System.out.println("SUCCESSFULLY PULLED FROM THE WEBSERVICE!!");
                 } else {
                     System.out.println("NO RESULTS");
