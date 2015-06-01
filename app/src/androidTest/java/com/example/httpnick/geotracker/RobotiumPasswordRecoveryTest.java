8d33ba0efb5cb63d2e19033107398088fa92c09b
Nick Duncan <nick.b.duncan@gmail.com>
2015-05-31 18:19:37 -0700

    Added back Matt's RobotiumPasswordRecoveryTest.java file. I accidentally messed up and deleted it in my previous push. ALL CREDIT OF THIS FILE GOES TO MATT. NOT ME.

---
+++ app/src/androidTest/java/com/example/httpnick/geotracker/RobotiumPasswordRecoveryTest.java
@@ -0,0 +1,58 @@
+package com.example.httpnick.geotracker;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.robotium.solo.Solo;
+
+/**
+ * A test class to check the Password Recovery function.
+ * @author Matthew Moore
+ * 5/18/15.
+ */
+public class RobotiumPasswordRecoveryTest extends ActivityInstrumentationTestCase2<PasswordRecovery> {
+
+    private Solo solo;
+
+    /**
+     * Constructor passes in the PasswordRecovery.java class to ActivityInstrumentationTestCase2
+     */
+    public RobotiumPasswordRecoveryTest() {
+        super(PasswordRecovery.class);
+    }
+
+    /**
+     * Call super.setUp() and initialize Solo solo.
+     * @throws Exception
+     */
+    public void setUp() throws Exception {
+        super.setUp();
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+
+    /**
+     * Enter an invalid e-mail.
+     * Test for alert dialog.
+     * Enter a valid e-mail.
+     * Test for success.
+     */
+    public void testRecovery() {
+        solo.enterText(0, "asdfa@gmail.com");
+        solo.clickOnButton("Send email");
+        solo.clickOnButton("OK");
+        solo.clearEditText(0);
+        solo.enterText(0, "moorem27@uw.edu");
+        solo.clickOnButton("Send email");
+        boolean recovered = solo.searchText("Success!");
+        assertTrue("Successfully reset password", recovered);
+    }
+
+    /**
+     * Exit once the testing is complete.
+     * @throws Exception
+     */
+    public void tearDown() throws Exception {
+        solo.clickOnButton("OK");
+        solo.finishOpenedActivities();
+    }
+}
